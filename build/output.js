2d29e451b9487f5d3d76f9589e1c23d0ecabde31
Soraismus <tertius_lupus@yahoo.com>
2016-07-27 01:08:04 -0400

    Check in current state.

--- build/output.js
+++ build/output.js
@@ -498,6 +498,47 @@ function initialize(config) {
 
 module.exports = initialize;
 
+
+function f0() {
+  var ratio;
+
+  var viewport = document.getElementById('erl-viewport');
+  var xTrack = document.getElementById('erl-x-scroll-track');
+  var yTrack = document.getElementById('erl-y-scroll-track');
+  var xThumb = document.getElementById('erl-x-scroll-thumb');
+  var yThumb = document.getElementById('erl-y-scroll-thumb');
+
+  var viewportWidth = viewport.offsetWidth;
+  var terminalWidth = viewport.scrollWidth;
+
+  if (viewportWidth < terminalWidth) {
+    console.log('part is hidden');
+  }
+}
+
+/*
+`viewport.offsetWidth` is the size of the 'viewport'.
+`viewport.scrollWidth` is the size of the underlying 'terminal'.
+
+The scroll track's length should be offsetWidth / scrollWdith.
+
+
+var X_SCROLLBAR = DIV(
+  {
+    id: 'erl-x-scroll-track',
+    classes: {
+      'erl-x-scroll-track': true,
+      'erl-scroll-track': true
+    }
+  },
+  DIV({
+    id: 'erl-x-scroll-thumb',
+    classes: {
+      'erl-x-scroll-thumb': true,
+      'erl-scroll-thumb': true
+
+*/
+
 },{"./control/initializeControl":10,"./models/getInitialModel":15,"./render":20,"./subscribe":21,"./view/control/recreateConsole":24,"./view/initializeView":26,"./view/initializeViewDynamics":27}],12:[function(require,module,exports){
 var create = require('../types/createFrame');
 
@@ -891,10 +932,47 @@ function render(_viewModel, rootChild, getCursor, controlConfig) {
 
     controlConfig.viewport = model;
     viewModel = newViewModel;
+
+    f0();
+
   };
 }
 
 module.exports = render;
+
+
+
+function f0() {
+  var viewport = document.getElementsByClassName('erl-viewport')[0];
+  var xTrack = document.getElementById('erl-x-scroll-track');
+  var yTrack = document.getElementById('erl-y-scroll-track');
+  var xThumb = document.getElementById('erl-x-scroll-thumb');
+  var yThumb = document.getElementById('erl-y-scroll-thumb');
+  var cursor = document.getElementById('erl-cursor');
+  var prompt = document.getElementById('erl-prompt');
+
+  var xTrackWidth = xTrack.offsetWidth;
+  var xThumbWidth = xThumb.offsetWidth;
+  var viewportWidth = viewport.offsetWidth;
+  var terminalWidth = viewport.scrollWidth;
+
+  if (viewportWidth < terminalWidth) {
+    var fullPromptOffsetWidth = prompt.offsetLeft + prompt.offsetWidth;
+
+    var viewportRatio = viewportWidth / terminalWidth;
+    var viewportPercentage = (100 * viewportRatio) + '%';
+
+    var ullage = xTrackWidth - xThumbWidth;
+    var xPosition = cursor.offsetLeft - fullPromptOffsetWidth;
+    var cursorRatio = (xPosition / terminalWidth) * (ullage / xTrackWidth);
+    var cursorPercentage = (100 * cursorRatio) + '%';
+
+    var xThumbStyle = xThumb.style;
+    xThumbStyle.left = cursorPercentage;
+    xThumbStyle.width = viewportPercentage;
+    xThumbStyle.visibility = 'visible';
+  }
+}
 
 },{"./../lib/interpreter":2,"./view/control/diff":23,"./view/control/recreateConsole":24,"./view/control/scroll":25}],21:[function(require,module,exports){
 function subscribe(eventType, eventHandler) {
